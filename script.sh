57cd5c50fbdc01d41796d09627440fa7d727d900
akshay.mathur <akshay.mathur@birlasoft.com>
2018-07-05 12:26:20 +0530

    script update to convert ipynb to py

--- script.sh
+++ script.sh
@@ -1,2 +1,7 @@
+echo "Converting ipynb to py"
+jupyter nbconvert *.ipynb --to python
+echo "Successfully converted!"
+echo "Training the model"
 sudo /home/ec2-user/anaconda3/bin/python train.py
+echo "Running validation"
 sudo /home/ec2-user/anaconda3/bin/python validate.py
